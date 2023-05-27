ebd2ca4df072aaad26ecfa44b95f0168935aa87b
phamichanh <anh.pi270390@gmail.com>
2023-05-28 01:14:35 +0900

    feat: create BtnHex component

---
+++ app/components/BtnHex/index.tsx
@@ -0,0 +1,23 @@
+import styles from "./index.module.scss";
+
+type BtnIcon = "knife" | "cup";
+
+function HexagonalButton(props: {
+  label?: string;
+  icon?: BtnIcon;
+  href?: string;
+}) {
+  const { label = "Button", icon = "knife", href = "#" } = props;
+  const classIcon = styles[`btn-hex__icon-${icon}`];
+  return (
+    <a
+      href={href}
+      className={`${styles["btn-hex"]} flex flex-col items-center justify-center`}
+    >
+      <span className={`${styles["btn-hex__icon"]} ${classIcon}`}></span>
+      <p className={styles["btn-hex__label"]}>{label}</p>
+    </a>
+  );
+}
+
+export default HexagonalButton;
