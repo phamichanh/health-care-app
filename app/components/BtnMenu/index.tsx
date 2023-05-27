7c08410449fae42296a767c701f2977d51ccdf7f
phamichanh <anh.pi270390@gmail.com>
2023-05-28 03:23:32 +0900

    feat: create BtnMenu component

---
+++ app/components/BtnMenu/index.tsx
@@ -0,0 +1,19 @@
+import styles from "./index.module.scss";
+
+type BtnIcon = "memo" | "challenge" | "info";
+
+function BtnMenu(props: { label?: string; icon?: BtnIcon; href?: string }) {
+  const { label = "Button", icon = "memo", href = "#" } = props;
+  const classIcon = styles[`btn-menu__icon-${icon}`];
+  return (
+    <a
+      href={href}
+      className={`${styles["btn-menu"]} flex items-center justify-center`}
+    >
+      <span className={`${styles["btn-menu__icon"]} ${classIcon} mr-2`}></span>
+      <p className={styles["btn-menu__label"]}>{label}</p>
+    </a>
+  );
+}
+
+export default BtnMenu;
