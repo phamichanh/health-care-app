15914f8068b481beaaf970f961649bb45f0b0659
phamichanh <anh.pi270390@gmail.com>
2023-05-28 23:43:32 +0900

    feat: create my-record page

---
+++ app/my-record/page.tsx
@@ -0,0 +1,42 @@
+import styles from "./my-record.module.scss";
+import Recommend from "../components/Recommend";
+import BodyGraph from "../components/BodyGraph";
+import Diary from "../components/Diary";
+
+export default function MyRecord() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section className="section flex justify-between">
+        <Recommend />
+        <Recommend
+          title="my exercise"
+          btnLabel="自分の運動の記録"
+          src="/img/recommends/MyRecommend-2.jpg"
+          alt="runner"
+        />
+        <Recommend
+          title="my diary"
+          btnLabel="自分の日記"
+          src="/img/recommends/MyRecommend-3.jpg"
+          alt="using phone"
+        />
+      </section>
+      <section className={`${styles["body-record"]} section`}>
+        <BodyGraph width={960} height={210} />
+      </section>
+      <section className={`${styles["my-exercise"]} section`}></section>
+      <section className={`${styles["my-diary"]} section`}>
+        <p className={`${styles["my-diary__title"]}`}>MY DIARY</p>
+        <div className="flex flex-wrap justify-between">
+          {(() => {
+            const elements = [];
+            for (let i = 0; i < 8; i++) {
+              elements.push(<Diary />);
+            }
+            return elements;
+          })()}
+        </div>
+      </section>
+    </main>
+  );
+}
