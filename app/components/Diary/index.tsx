76aa003e2794b3035d50f0a7fd2dcd5ed3e51474
phamichanh <anh.pi270390@gmail.com>
2023-05-28 23:43:14 +0900

    refactor: adjust color

--- app/components/Diary/index.tsx
+++ app/components/Diary/index.tsx
@@ -7,7 +7,7 @@ function Diary(props: { date?: string; time?: string; text?: string }) {
     text = "私の日記の記録が一部表示されます。\nテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト…",
   } = props;
   return (
-    <div className={`${styles.diary} my-1 p-4`}>
+    <div className={`${styles.diary} my-2 p-4`}>
       <p className={`${styles.diary__date}`}>{date}</p>
       <p className={`${styles.diary__time} mb-2`}>{time}</p>
       <p className={`${styles.diary__text}`}>{text}</p>
